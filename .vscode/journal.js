3ff6a79257b9477dc7069b3ee63a37da986d5baa
Eli <earnold2010@gmail.com>
2022-07-23 22:41:23 -0500

    Update journal.js

--- .vscode/journal.js
+++ .vscode/journal.js
@@ -93,7 +93,60 @@ var JOURNAL = [
 
 // This makes sure the data is exported in node.js —
 // `require('./path/to/journal.js')` will get you the array.
+
 if (typeof module != "undefined" && module.exports && (typeof window == "undefined" || window.exports != exports))
   module.exports = JOURNAL;
 if (typeof global != "undefined" && !global.JOURNAL)
   global.JOURNAL = JOURNAL;
+
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i], index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+//console.log(tableFor("pizza", JOURNAL));
+
+//for (let entry of JOURNAL) {
+//  console.log(`${entry.events.length} events.`);
+//}
+
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+return events;
+}
+
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table [1]) / 
+  Math.sqrt((table[2] + table[3]) * 
+  (table[0] + table[1]) *
+  (table[1] + table[3]) * 
+  (table[0] + table[2]));
+}
+
+for (let event of journalEvents(JOURNAL)) {
+  let correlation = phi(tableFor(event, JOURNAL));
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(event + ":", correlation);
+  }
+}
+
+for (let entry of JOURNAL) {
+  if (entry.events.includes("peanuts") &&
+  !entry.events.includes("brushed teeth")) {
+    entry.events.push("peanut teeth");
+  }
+}
+console.log(phi(tableFor("peanut teeth", JOURNAL)));
